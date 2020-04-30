e45fbea4c874ab62de12933f4411087c8461ea9c
RDavis36 <rdavis36@slb.com>
2020-04-30 10:55:34 -0500

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: "/test",
+        name: "Test",
+        component: () => import('@/components/Test')
+    },
+    {
+        path: "/",
+        name: "Home",
+        component: () => import('@/components/HelloWorld')
+    }
+]
+
+const router = new VueRouter({
+    mode: "history",
+    routes: routes,
+    base: process.env.BASE_URL
+})
+
+export default router
